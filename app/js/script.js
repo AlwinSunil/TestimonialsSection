dcc4ccd9ff91ddf0cb1b1caf94fac216e7ca6ae2
AlwinSunil <alv12alwin@gmail.com>
2021-03-31 17:55:12 +0530

    v1.0 - Fixed Bugs

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   AOS.init();
   window.scrollTo(0, 0);
+  document.getElementById("body").style.height = "auto";
   const loader = document.getElementById("loader-wrapper");
   loader.style.opacity = 0;
 };
